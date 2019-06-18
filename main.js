f1a14d582193dfa1ab454b8032a0fea6c352e0c1
ivandoroshenko <ivandoroshenko1988@gmail.com>
2019-06-19 00:05:15 +0300

    Ivan finished 1,2 challenges. Finished lesson attr binding

---
+++ main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks",
+        image: "./assets/vmSocks-green-onWhite.jpg",
+        altText: "A pair of socks",
+        href: "https://ivandoroshenko.github.io/portfolio/src/"
+    }
+})
