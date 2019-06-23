e73912b6e17a152a0b77ad47c9f54cc83b0a94f1
ivandoroshenko <ivandoroshenko1988@gmail.com>
2019-06-23 22:17:12 +0300

    Conditional rendering

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var app = new Vue({
         product: "Socks",
         image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
-        href: "https://ivandoroshenko.github.io/portfolio/src/"
+        inventory: 0,
+        onSale: false
     }
 })
