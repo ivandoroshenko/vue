8cc727647987a233c105439e83cdc57b3c4a936e
ivandoroshenko <ivandoroshenko1988@gmail.com>
2019-06-24 12:21:13 +0300

    class & style bidning

--- main.js
+++ main.js
@@ -9,13 +9,27 @@ var app = new Vue({
         variants: [
             {
                 variantId: 2234,
-                variantColor: "green"
+                variantColor: "green",
+                variantImage: "./assets/vmSocks-green-onWhite.jpg"
             },
             {
                 variantId: 2235,
-                variantColor: "blue"
+                variantColor: "blue",
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg"
             }
         ],
-        sizes: ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+        sizes: ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+        cart: 0,
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateProduct(variantImage) {
+                this.image = variantImage
+        },
+        dercCart() {
+            this.cart -= 1
+        }
     }
 })
