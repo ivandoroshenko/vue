e202495e39f3538218f2c46eb7920848e79696f1
ivandoroshenko <ivandoroshenko1988@gmail.com>
2019-06-28 18:40:47 +0300

    finished computed properties

--- main.js
+++ main.js
@@ -2,31 +2,51 @@ var app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        image: "./assets/vmSocks-green-onWhite.jpg",
+        brand: 'Vue Craftery',
+        selectedVariant: 0,
         altText: "A pair of socks",
-        inStock: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
                 variantColor: "green",
-                variantImage: "./assets/vmSocks-green-onWhite.jpg"
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: "blue",
-                variantImage: "./assets/vmSocks-blue-onWhite.jpg"
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
             }
         ],
         sizes: ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
         cart: 0,
+        onSale: true
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale() {
+            if (this.onSale) {
+                return this.brand +  ' ' + this.product + ' '+ 'are on sale!'
+            }
+        }
     },
     methods: {
         addToCart() {
             this.cart += 1
         },
-        updateProduct(variantImage) {
-                this.image = variantImage
+        updateProduct(index) {
+                this.selectedVariant = index
+                
         },
         dercCart() {
             this.cart -= 1
